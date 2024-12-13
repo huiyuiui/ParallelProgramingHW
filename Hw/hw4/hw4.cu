491357d7cb92f8447cdcbfd78c08767aa81b72d3
huiyuiui <chen1741954267@gmail.com>
2024-12-13 15:46:43 +0800

    hw4 done

--- Hw/hw4/hw4.cu
+++ Hw/hw4/hw4.cu
@@ -16,6 +16,8 @@ cudaDeviceProp prop;
 int B, N, d;
 float *Q, *K, *V, *O;
 float *dev_Q, *dev_K, *dev_V, *dev_O;
+float *l, *m;
+float *dev_l, *dev_m;
 
 void input(char *input_filename);
 
@@ -27,7 +29,7 @@ double getTimeStamp() {
     return (double) tv.tv_usec/1000000 + tv.tv_sec;
 }
 
-__global__ void flash_attention_kernel(float* q, float* k, float* v, float* o, float* l, float* m, int d);
+__global__ void flash_attention_kernel(float* __restrict__ q, float* __restrict__ k, float*  __restrict__ v, float* o, float* l, float* m, int d, int block_x);
 
 int main(int argc, char* argv[]) {
     if (argc != 3) {
@@ -39,12 +41,18 @@ int main(int argc, char* argv[]) {
 
     cudaGetDeviceProperties(&prop, DEV_NO);
     printf("maxThreasPerBlock = %d, sharedMemPerBlock = %d\n", prop.maxThreadsPerBlock, prop.sharedMemPerBlock);
-    
+
+    // init l and m
+    l = (float *)malloc(N * sizeof(float));
+    m = (float *)malloc(N * sizeof(float));
+
     // cuda malloc memory
     cudaMalloc((void**)&dev_Q, B * N * d * sizeof(float));
     cudaMalloc((void**)&dev_K, B * N * d * sizeof(float));
     cudaMalloc((void**)&dev_V, B * N * d * sizeof(float));
     cudaMalloc((void**)&dev_O, B * N * d * sizeof(float));
+    cudaMalloc((void**)&dev_l, N * sizeof(float));
+    cudaMalloc((void**)&dev_m, N * sizeof(float));
 
     // copy host memory to cuda
     cudaMemcpy(dev_Q, Q, B * N * d * sizeof(float), cudaMemcpyHostToDevice);
@@ -54,42 +62,34 @@ int main(int argc, char* argv[]) {
 
     // kernel parameters
     dim3 blockSize(BLOCK_SIZE, BLOCK_SIZE);
-    dim3 blockNum(N / BLOCK_SIZE, N / BLOCK_SIZE);
+    dim3 blockNum(1, N / BLOCK_SIZE); // block_x_num is 1 because dependency exist between columns
 
     double start, end;
     start = getTimeStamp();
 
     // flash attention
     for (int i = 0; i < B; i++){
-        // init l and m
-        float *l = (float *)malloc(N * sizeof(float));
-        float *m = (float *)malloc(N * sizeof(float));
+        // re-initialize l and m
         memset(l, 0x00, N * sizeof(float));
         for (int j = 0; j < N; j++) {
-            m[j] = -FLT_MAX;
+            m[j] = FLT_MIN;
         }
 
         // copy l and m to device
-        float *dev_l, *dev_m;
-        cudaMalloc((void**)&dev_l, N * sizeof(float));
-        cudaMalloc((void**)&dev_m, N * sizeof(float));
         cudaMemcpy(dev_l, l, N * sizeof(float), cudaMemcpyHostToDevice);
         cudaMemcpy(dev_m, m, N * sizeof(float), cudaMemcpyHostToDevice);
 
         // kernel call
-        flash_attention_kernel<<<blockNum, blockSize>>>(
-            dev_Q + i * N * d, 
-            dev_K + i * N * d, 
-            dev_V + i * N * d, 
-            dev_O + i * N * d,
-            dev_l, dev_m, d
-        );
-
-        //free memory of l and m 
-        free(l);
-        free(m);
-        cudaFree(dev_l);
-        cudaFree(dev_m);
+        for (int j = 0; j < N / BLOCK_SIZE; j++) // we need a for loop to traverse columns
+        {
+            flash_attention_kernel<<<blockNum, blockSize>>>(
+                dev_Q + i * N * d, 
+                dev_K + i * N * d, 
+                dev_V + i * N * d, 
+                dev_O + i * N * d,
+                dev_l, dev_m, d, j
+            );
+        }
     }
 
     end = getTimeStamp();
@@ -102,10 +102,14 @@ int main(int argc, char* argv[]) {
     output(argv[2]);
 
     // free memory
+    free(l);
+    free(m);
     cudaFree(dev_Q);
     cudaFree(dev_K);
     cudaFree(dev_V);
     cudaFree(dev_O);
+    cudaFree(dev_l);
+    cudaFree(dev_m);
 
     return 0;
 }
@@ -145,10 +149,10 @@ void output(char *output_filename) {
     fclose(file);
 }
 
-__global__ void flash_attention_kernel(float* q, float* k, float* v, float* o, float* l, float* m, int d){
+__global__ void flash_attention_kernel(float* __restrict__ q, float* __restrict__ k, float*  __restrict__ v, float* o, float* l, float* m, int d, int block_x){
     int thread_x = threadIdx.x;
     int thread_y = threadIdx.y;
-    int block_j = blockIdx.x;
+    int block_j = block_x;
     int block_i = blockIdx.y;
     int dim = d;
     int br = BLOCK_SIZE, bc = BLOCK_SIZE;
@@ -186,39 +190,46 @@ __global__ void flash_attention_kernel(float* q, float* k, float* v, float* o, f
     }
     __syncthreads();
 
+
     /* QKDotAndScalar(sij, qi, kj, br, bc, 1.0/sqrt(d)) */
-    sij[thread_y * bc + thread_x] = 0.0F;
+    float val = 0.0F;
     for (int t = 0; t < dim; t++)
     {
-        sij[thread_y * bc + thread_x] += qi[thread_y * dim + t] * kj[thread_x * dim + t];
+        val += qi[thread_y * dim + t] * kj[thread_x * dim + t];
     }
-    sij[thread_y * bc + thread_x] *= (1.0 / sqrtf(dim));
+    sij[thread_y * bc + thread_x] = val * (1.0 / sqrtf(dim));
     __syncthreads();
+
 
     /* RowMax(mij, sij, br, bc) */
     if(thread_x == 0){ // only need one thread for each row to compute row max
-        mij[thread_y] = sij[thread_y * bc];
+        float max_val = FLT_MIN;
         for (int j = 0; j < bc; j++)
         {
-            mij[thread_y] = max(mij[thread_y], sij[thread_y * bc + j]);
+            max_val = max(max_val, sij[thread_y * bc + j]);
         }
+        mij[thread_y] = max_val;
     }
     __syncthreads();
+
 
     /*  MinusMaxAndExp(pij, sij, mij, br, bc) */
     pij[thread_y * bc + thread_x] = expf(sij[thread_y * bc + thread_x] - mij[thread_y]);
     __syncthreads();
 
+
     /* RowSum(lij, pij, br, bc) */
     if(thread_x == 0){ // only need one thread for each row to sum up row
-        lij[thread_y] = 0.0F;
+        float val = 0.0F;
         for (int j = 0; j < bc; j++)
         {
-            lij[thread_y] += pij[thread_y * bc + j];
+            val += pij[thread_y * bc + j];
         }
+        lij[thread_y] = val;
     }
     __syncthreads();
-    
+
+
     /* UpdateMiLiOi(mi, li, oi, mij, lij, pij, vj, br, bc) */
     if(thread_x == 0){ // only need one thread for each row to do this
         mi_new[thread_y] = max(mi[thread_y], mij[thread_y]);
@@ -226,18 +237,24 @@ __global__ void flash_attention_kernel(float* q, float* k, float* v, float* o, f
                          + expf(mij[thread_y] - mi_new[thread_y]) * lij[thread_y];
 
         __syncthreads();
-
-        for (int j = 0; j < dim; j++)
-        {
-            float pv = 0.0F;
-            for (int t = 0; t < bc; t++)
-            {
-                pv += pij[thread_y * bc + t] * vj[t * dim + j];
-            }
-            oi[thread_y * dim + j] = (li[thread_y] * expf(mi[thread_y] - mi_new[thread_y]) * oi[thread_y * dim + j]
-                                    + expf(mij[thread_y] - mi_new[thread_y]) * pv) / li_new[thread_y];
-        }
     }
+    // use thread_x replace dim
+    // for (int j = 0; j < dim; j++) -> thread_x = j
+    float first_item = li[thread_y] * expf(mi[thread_y] - mi_new[thread_y]);
+    float second_item = expf(mij[thread_y] - mi_new[thread_y]);
+    float pv_1 = 0.0F;
+    float pv_2 = 0.0F;
+    for (int t = 0; t < bc; t++)
+    {
+        pv_1 += pij[thread_y * bc + t] * vj[t * dim + thread_x];
+        if(dim == 64)
+            pv_2 += pij[thread_y * bc + t] * vj[t * dim + (bc + thread_x)];
+    }
+    oi[thread_y * dim + thread_x] = (first_item * oi[thread_y * dim + thread_x] + second_item * pv_1) / li_new[thread_y];
+    if(dim == 64)
+        oi[thread_y * dim + (bc + thread_x)] = (first_item * oi[thread_y * dim + (bc + thread_x)] + second_item * pv_2) / li_new[thread_y];
+    
+    
     __syncthreads();
 
     // copy memory back
